5d7472f35629836490c9f119a022b7d18183e8d3
Marcin Drozd <marcindrozd@me.com>
2018-08-10 15:29:02 +0200

    Add basic auth for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"] if Rails.env.production?
 end
