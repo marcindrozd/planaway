ea0623e013472cf480531d1eee0451c2fbfee3be
Marcin Drozd <marcindrozd@me.com>
2019-04-18 13:20:07 +0200

    Add sign up page components

--- app/javascript/pages/SignUp/index.js
+++ app/javascript/pages/SignUp/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import {
+  Grid,
+  Card,
+  CardHeader,
   Typography,
 } from '@material-ui/core';
+
+import styles from './styles.module.scss';
 
 const SignUp = () => {
   const { t } = useTranslation();
 
   return (
-    <Typography>
-      {t('Sign Up')}
-    </Typography>
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      className={styles.container}
+    >
+      <Card className={styles.card}>
+        <CardHeader title={t('Sign Up')} />
+      </Card>
+    </Grid>
   );
 };
 
